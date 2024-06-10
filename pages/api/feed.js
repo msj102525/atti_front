efbbfe4f8cf1bb45ecb7a536d3a9bca092798293
msj102525 <msj102525@gmail.com>
2024-06-10 19:47:50 +0900

    피드 작성 폼 완료 문승종 0610

---
+++ pages/api/feed.js
@@ -0,0 +1,12 @@
+import axios from './axiosApi';
+
+export const postFeed = (postData) =>{
+    console.log("AxiosPostData : ", postData);
+    // return axios.post("/feed",postData)
+    // .then(res =>{
+    //     console.log("res :", res);
+    //     return res;
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+}
