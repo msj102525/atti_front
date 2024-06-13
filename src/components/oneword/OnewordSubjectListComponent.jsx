ea5c83110f1b41ba383937df608cc63878d5cc3d
ictedu29 <tactius90@gmail.com>
2024-06-13 11:52:07 +0900

    오늘 한 줄 주제 수정 중 / 2024-06-13 / 남길현

--- src/components/oneword/OnewordSubjectListComponent.jsx
+++ src/components/oneword/OnewordSubjectListComponent.jsx
@@ -6,13 +6,18 @@ import { ko } from 'date-fns/locale';
 const OnewordSubjectListComponent = observer(({ onewordsubject, isPinned = false, onNoticeClick  }) => {
     const relativeTime = (isoDate) => formatDistanceToNow(parseISO(isoDate), { addSuffix: true, locale: ko });
 
+    const date = new Date(onewordsubject.owsjWriteDate);
+
+    // 원하는 형식에 따라 날짜를 format
+    const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
     return (
         <tr onClick={onNoticeClick} style={{ background: isPinned ? "skyblue" : "none" }}>
             {/* <td style={{ width: "5vw", textAlign: "center" }}>{isPinned ? "공지" : "일반"}</td> */}
-            <td style={{ textAlign: "left" }}>{onwordsubject.subject}</td>
-            <td style={{ width: "10vw", textAlign: "center" }}>{onwordsubject.writer}</td>
-            <td style={{ width: "10vw", textAlign: "center" }}>{onwordsubject.write_date}</td>
-            {/* <td style={{ width: "7vw", textAlign: "center" }}>{relativeTime(notice.createdAt)}</td> */}
+            <td style={{ textAlign: "left" }}>{onewordsubject.owsjSubject}</td>
+            <td style={{ width: "10vw", textAlign: "center" }}>{onewordsubject.owsjWriter}</td>
+            <td style={{ width: "10vw", textAlign: "center" }}>{formattedDate}</td>
+            {/* <td style={{ width: "7vw", textAlign: "center" }}>{relativeTime(onewordsubject.owsjWriteDate)}</td> */}
         </tr>
     );
 });
