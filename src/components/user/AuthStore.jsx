03d9c0174e4acf2b6806c59c82426c68b1af908a
JiWoong Han <smkr96@naver.com>
2024-06-21 11:24:17 +0900

    로그인 오류 해결 . 클라이언트에서 렌더링 될때까지 랜더링안함

--- src/components/user/AuthStore.jsx
+++ src/components/user/AuthStore.jsx
@@ -1,91 +1,100 @@
 /*src/components/user/AuthStatus.js*/
-import React, { useEffect } from 'react';
-import { observer } from 'mobx-react-lite';
-import { useRouter } from 'next/router';
-import { authStore } from '@/pages/stores/authStore';
-import { logout, logoutkakao } from '@/api/user/user';
-import { getUserData  } from '@/api/user/userApi';
+import React, { useEffect, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { useRouter } from "next/router";
+import { authStore } from "@/pages/stores/authStore";
+import { logout, logoutkakao } from "@/api/user/user";
+import { getUserData } from "@/api/user/userApi";
+import NavButton from "../doctor/NavButton";
 
 const AuthStatus = observer(() => {
   const router = useRouter();
+  const [userType, setUserType] = useState("");
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setUserType(authStore.userType);
     authStore.checkLoggedIn();
+    setIsClient(true); // 클라이언트에서만 true로 설정
   }, []);
 
   const redirectToUserTypePage = () => {
-    const userType = authStore.userType;
-    if (userType === 'A') {
-      router.push('/admin/memberList');
-    } else if (userType === 'U') {
-      router.push('/');
-    } else if (userType === 'D') {
-      router.push('/');
+    if (userType === "A") {
+      router.push("/admin/memberList");
+    } else if (userType === "U") {
+      router.push("/");
+    } else if (userType === "D") {
+      router.push("/");
     } else {
-      router.push('/');
+      router.push("/");
     }
   };
 
   const handleLoginClick = async () => {
     try {
       if (!authStore.loggedIn && !authStore.socialLoggedIn) {
-        router.push('/login'); // 로그인 페이지로 이동
+        router.push("/login"); // 로그인 페이지로 이동
       } else if (authStore.socialLoggedIn) {
         await logoutkakao();
         authStore.setSocialLoggedIn(false); // 소셜 로그아웃 처리
-        router.push('/'); 
+        router.push("/");
       } else {
         await logout();
         authStore.setLoggedIn(false); // 일반 로그아웃 처리
-        router.push('/'); 
+        router.push("/");
       }
     } catch (error) {
-      console.error('로그아웃 실패:', error);
+      console.error("로그아웃 실패:", error);
     }
   };
 
   const handleMyPageClick = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       const userData = await getUserData(token);
       router.push({
-        pathname: '/user/mypage', // mypage 페이지로 이동
+        pathname: "/user/mypage", // mypage 페이지로 이동
         query: { userId: JSON.stringify(userData) },
       });
     } catch (error) {
-      console.error('사용자 데이터 요청 실패:', error);
+      console.error("사용자 데이터 요청 실패:", error);
     }
   };
 
   const handleMove = async () => {
-    router.push('/chat/chatList');
+    router.push("/chat/chatList");
   };
 
+  if (!isClient) {
+    // 클라이언트에서 렌더링하기 전까지는 아무것도 렌더링하지 않음
+    return null;
+  }
 
   return (
-    <div>
-      <button onClick={handleLoginClick}>
-        {authStore.loggedIn || authStore.socialLoggedIn ? '로그아웃' : '로그인'}
-      </button>
-      <br/>
-      {(authStore.loggedIn || authStore.socialLoggedIn) && (
-        <>
-          <div>
-          <button onClick={handleMyPageClick}>
-            MyPage
-          </button>
-          </div>
-          <button onClick={handleMove}>
-            채팅 리스트
-          </button>
-          <p>{authStore.userName}님</p>
-        </>
-      )}
-      {!authStore.loggedIn && !authStore.socialLoggedIn && (
-        <button onClick={() => router.push('/signup')}>
-          회원가입
+    <div className="flex">
+      <div className="flex flex-col items-center justify-center mr-8">
+        <NavButton userType={userType} />
+        <button onClick={handleMove}>채팅 리스트</button>
+      </div>
+      <div>
+        <button onClick={handleLoginClick}>
+          {authStore.loggedIn || authStore.socialLoggedIn
+            ? "로그아웃"
+            : "로그인"}
         </button>
-      )}
+        <br />
+        {(authStore.loggedIn || authStore.socialLoggedIn) && (
+          <>
+            <div>
+              <button onClick={handleMyPageClick}>MyPage</button>
+            </div>
+            <p>{authStore.userName}님</p>
+          </>
+        )}
+        {!authStore.loggedIn && !authStore.socialLoggedIn && (
+          <button onClick={() => router.push("/signup")}>회원가입</button>
+        )}
+      </div>
     </div>
   );
 });
