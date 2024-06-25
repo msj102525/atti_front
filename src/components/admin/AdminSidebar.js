e1c3ee9e7bed577fb6b9caf976b07086eb27f323
ictedu29 <tactius90@gmail.com>
2024-06-25 13:06:08 +0900

    관리자 페이지 수정 / 2024-06-25 / 남길현

--- src/components/admin/AdminSidebar.js
+++ src/components/admin/AdminSidebar.js
@@ -15,22 +15,23 @@ const Sidebar = () => {
     };
 
     return (
-        <div className={styles.sidebar }>
-            <h3 className={styles.menuTitle}>관리자 메뉴</h3>
-            <div className={styles.menu}>
-                <ul>
-                    <li onClick={() => toggleMenu('member')}>
-                        <span>회원 관리</span>
-                        <span className={styles.arrow}>{openMenu === 'member' ? '▲' : '▼'}</span>
-                    </li>
+        <div className='py-[20px] min-h-[100%]'>
+            <div className={styles.sidebar}>
+                <h3 className={styles.menuTitle}>관리자 메뉴</h3>
+                <div className={styles.menu}>
+                    <ul>
+                        <li onClick={() => toggleMenu('member')}>
+                            <span>회원 관리</span>
+                            <span className={styles.arrow}>{openMenu === 'member' ? '▲' : '▼'}</span>
+                        </li>
                         <ul className={`${openMenu === 'member' ? styles.open : ''}`}>
                             <li onClick={() => handleNavigation('/admin/memberList')}>회원 리스트</li>
                             <li onClick={() => handleNavigation('/admin/suspensionMemberList')}>정지 회원</li>
                         </ul>
-                    <li onClick={() => toggleMenu('post')}>
-                        <span>게시글 관리</span>
-                        <span className={styles.arrow}>{openMenu === 'post' ? '▲' : '▼'}</span>
-                    </li>
+                        <li onClick={() => toggleMenu('post')}>
+                            <span>게시글 관리</span>
+                            <span className={styles.arrow}>{openMenu === 'post' ? '▲' : '▼'}</span>
+                        </li>
                         <ul className={`${openMenu === 'post' ? styles.open : ''}`}>
                             <li onClick={() => handleNavigation('/admin/communityAdminVersion')}>커뮤니티</li>
                             <li onClick={() => handleNavigation('/admin/noticeAdminVersion')}>공지사항</li>
@@ -38,15 +39,16 @@ const Sidebar = () => {
                             <li onClick={() => handleNavigation('/admin/inquiryAdminVersion')}>문의하기</li>
                             <li onClick={() => handleNavigation('/admin/onewordAdminVersion')}>오늘 한 줄</li>
                         </ul>
-                    <li onClick={() => toggleMenu('pay')}>
-                        <span>결제관리</span>
-                        <span className={styles.arrow}>{openMenu === 'pay' ? '▲' : '▼'}</span>
-                    </li>
+                        <li onClick={() => toggleMenu('pay')}>
+                            <span>결제관리</span>
+                            <span className={styles.arrow}>{openMenu === 'pay' ? '▲' : '▼'}</span>
+                        </li>
                         <ul className={`${openMenu === 'pay' ? styles.open : ''}`}>
                             <li onClick={() => handleNavigation('/pay/list')}>결제내역</li>
                             <li onClick={() => handleNavigation('/pay/graph')}>매출보기</li>
                         </ul>
-                </ul>
+                    </ul>
+                </div>
             </div>
         </div>
     );
