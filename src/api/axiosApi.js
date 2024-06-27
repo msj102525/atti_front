00264d09634a1d767bbc714c23ac3c5346f9b281
DESKTOP-LV829ES\kyl15 <kyl1563@naver.com>
2024-06-27 23:30:13 +0900

    토큰갱신 수정!

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -20,6 +20,7 @@ instance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+
 // 로그아웃 함수
 const logout = async () => {
   const token = localStorage.getItem("token");
@@ -38,44 +39,54 @@ const logout = async () => {
   }
 };
 
+
 // 토큰 갱신 함수
 const refreshToken = async () => {
   try {
+    // 로컬 스토리지에서 refreshToken 가져오기
     const refreshToken = localStorage.getItem("refresh");
 
     if (!refreshToken) {
       throw new Error("Refresh token is missing");
     }
 
+    // /reissue 엔드포인트로 POST 요청 보내기
     const response = await instance.post("/reissue", null, {
       headers: {
         Authorization: `Bearer ${refreshToken}`,
       },
     });
 
+    // 서버 응답에서 새로운 AccessToken과 RefreshToken 추출
     const newAccessToken = response.headers["authorization"];
     const newRefreshToken = response.headers["refresh-token"];
 
+    // 새로운 AccessToken을 로컬 스토리지에 저장
     if (newAccessToken) {
       const pureAccessToken = newAccessToken.split(" ")[1];
       localStorage.setItem("token", pureAccessToken);
     }
 
+    // 새로운 RefreshToken을 로컬 스토리지에 저장
     if (newRefreshToken) {
       const pureRefreshToken = newRefreshToken.split(" ")[1];
       localStorage.setItem("refresh", pureRefreshToken);
     }
 
+    // 새로운 AccessToken 반환
     return newAccessToken ? newAccessToken.split(" ")[1] : null;
   } catch (error) {
+    // refreshToken이 만료된 경우 로그아웃 처리
     if (error.response && (error.response.data === "Refresh token expired" || error.response.status === 401)) {
       logout(); // 리프레시 토큰이 만료된 경우 로그아웃 처리
     } else {
+      // 기타 오류가 발생한 경우 콘솔에 에러 출력
       console.error("An error occurred:", error);
     }
     return null;
   }
 };
+
 
 // 응답 인터셉터 추가
 instance.interceptors.response.use(
