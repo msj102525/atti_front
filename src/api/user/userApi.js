7d69fbbf7119e84f5261d10f795a6fb44929e049
JiWoong Han <smkr96@naver.com>
2024-06-28 15:17:42 +0900

    마이페이지

--- src/api/user/userApi.js
+++ src/api/user/userApi.js
@@ -1,4 +1,4 @@
-import instance from '../axiosApi'; // Import your axios instance
+import instance from "../axiosApi"; // Import your axios instance
 
 const baseUrl = "/users";
 const baseUrl2 = "/auth";
@@ -6,28 +6,28 @@ const baseUrl2 = "/auth";
 // 유저 정보 수정
 export async function updateUser(userData) {
   try {
-    const response = await instance.put(baseUrl + '/update', userData, {
+    const response = await instance.put(baseUrl + "/update", userData, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     });
     return response.data;
   } catch (error) {
-    console.error('Failed to update user information:', error);
+    console.error("Failed to update user information:", error);
     if (error.response) {
-      console.error('Response data:', error.response.data);
-      console.error('Response status:', error.response.status);
-      console.error('Response headers:', error.response.headers);
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
     } else if (error.request) {
-      console.error('Request data:', error.request);
+      console.error("Request data:", error.request);
     } else {
-      console.error('Error message:', error.message);
+      console.error("Error message:", error.message);
     }
-    throw new Error('Failed to update user information.');
+    throw new Error("Failed to update user information.");
   }
 }
 export const snsUserUpdate = async (authStore) => {
-  console.log(authStore.userName, 'fewegw');
+  console.log(authStore.userName, "fewegw");
   try {
     // authStore에서 userData를 생성하고 loggedIn 필드를 삭제
     const userData = {
@@ -43,26 +43,30 @@ export const snsUserUpdate = async (authStore) => {
 
     console.log(userData);
 
-    const response = await instance.put(baseUrl2 + '/update', JSON.stringify(userData), {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json', 
-      },
-    });
+    const response = await instance.put(
+      baseUrl2 + "/update",
+      JSON.stringify(userData),
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     return response.data;
   } catch (error) {
-    console.error('Failed to update user information:', error);
+    console.error("Failed to update user information:", error);
     if (error.response) {
-      console.error('Response data:', error.response.data);
-      console.error('Response status:', error.response.status);
-      console.error('Response headers:', error.response.headers);
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
     } else if (error.request) {
-      console.error('Request data:', error.request);
+      console.error("Request data:", error.request);
     } else {
-      console.error('Error message:', error.message);
+      console.error("Error message:", error.message);
     }
-    throw new Error('Failed to update user information.');
+    throw new Error("Failed to update user information.");
   }
 };
 
@@ -71,7 +75,7 @@ export const deleteUser = async (userId) => {
   try {
     const response = await instance.delete(`${baseUrl}/deleteUser/${userId}`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     });
 
@@ -83,32 +87,32 @@ export const deleteUser = async (userId) => {
 
     return response.data;
   } catch (error) {
-    console.error('Failed to delete user:', error);
+    console.error("Failed to delete user:", error);
     if (error.response) {
-      console.error('Response data:', error.response.data);
-      console.error('Response status:', error.response.status);
-      console.error('Response headers:', error.response.headers);
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
     } else if (error.request) {
-      console.error('Request data:', error.request);
+      console.error("Request data:", error.request);
     } else {
-      console.error('Error message:', error.message);
+      console.error("Error message:", error.message);
     }
-    throw new Error('Failed to delete user.');
+    throw new Error("Failed to delete user.");
   }
-}
+};
 
 // 사용자 데이터 가져오기
-export const getUserData = async (token) => {
-  console.log(token,'토큰');
+export const getUserData = async (userId, token) => {
+  console.log(token, "토큰");
   try {
-    const response = await instance.get('/users',{
+    const response = await instance.get("/users/" + userId, {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
     return response.data;
   } catch (error) {
-    console.error('Failed to fetch user data:', error);
-    throw new Error('Failed to fetch user data.');
+    console.error("Failed to fetch user data:", error);
+    throw new Error("Failed to fetch user data.");
   }
 };
--- src/pages/user/mypage/index.js
+++ src/pages/user/mypage/index.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { observer } from 'mobx-react-lite';
-import Head from 'next/head';
-import Header from '@/pages/common/header';
-import styles from '@/styles/user/mypage.module.css';
+import React, { useState, useEffect, useRef } from "react";
+import { observer } from "mobx-react-lite";
+import Head from "next/head";
+import Header from "@/pages/common/header";
+import styles from "@/styles/user/mypage.module.css";
 import Modal2 from "@/components/common/Modal";
-import Footer from '@/pages/common/Footer';
-import { getUserData, updateUser, deleteUser } from '@/api/user/userApi';
-import { changePassword } from '@/api/user/find';
-import { uploadProfilePhoto, deleteProfilePhoto } from '@/api/doctor/doctorUpdate';
+import Footer from "@/pages/common/Footer";
+import { getUserData, updateUser, deleteUser } from "@/api/user/userApi";
+import { changePassword } from "@/api/user/find";
+import {
+  uploadProfilePhoto,
+  deleteProfilePhoto,
+} from "@/api/doctor/doctorUpdate";
 import { authStore } from "@/pages/stores/authStore";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 const Mypage = observer(() => {
   const [profileUrl, setProfileUrl] = useState(null);
   const fileInput = useRef(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [redirectPath, setRedirectPath] = useState(null); 
-  const [currentPassword, setCurrentPassword] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
+  const [redirectPath, setRedirectPath] = useState(null);
+  const [currentPassword, setCurrentPassword] = useState("");
   const router = useRouter();
 
   const serverImage = process.env.NEXT_PUBLIC_API_URL;
@@ -25,10 +28,9 @@ const Mypage = observer(() => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         const userId = authStore.userId;
         const data = await getUserData(userId, token);
-
         authStore.setUserId(data.userId);
         authStore.setUserName(data.userName);
         authStore.setNickName(data.nickName);
@@ -38,10 +40,11 @@ const Mypage = observer(() => {
         authStore.setGender(data.gender);
         authStore.setPhone(data.phone);
         authStore.setLoginType(data.loginType);
-        authStore.setBirthDate(data.birthDate)
+        authStore.setBirthDate(data.birthDate);
         setProfileUrl(data.profileUrl);
+        console.log("프로필 URL!!!!!!!!!!! : " + profileUrl);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
@@ -50,7 +53,7 @@ const Mypage = observer(() => {
 
   const handleUpdate = async () => {
     if (authStore.password !== authStore.confirmPassword) {
-      setModalMessage('비밀번호가 일치하지 않습니다.');
+      setModalMessage("비밀번호가 일치하지 않습니다.");
       setIsModalOpen(true);
       return;
     }
@@ -65,7 +68,7 @@ const Mypage = observer(() => {
         phone: authStore.phone,
         gender: authStore.gender,
         birthDate: authStore.birthDate,
-        profileUrl: authStore.profileUrl
+        profileUrl: authStore.profileUrl,
       };
       await updateUser(updatedUser);
 
@@ -74,31 +77,31 @@ const Mypage = observer(() => {
         await changePassword({
           currentPassword: currentPassword,
           newPassword: authStore.password,
-          userId: authStore.userId
+          userId: authStore.userId,
         });
       }
 
-      setModalMessage('수정 완료!');
+      setModalMessage("수정 완료!");
       setIsModalOpen(true);
-      setRedirectPath('/'); 
+      setRedirectPath("/");
     } catch (error) {
       console.error(error);
-      setModalMessage('수정 실패!');
+      setModalMessage("수정 실패!");
       setIsModalOpen(true);
     }
   };
 
   const handleCancel = async () => {
-    if (confirm('탈퇴 하시겠습니까?')) {
+    if (confirm("탈퇴 하시겠습니까?")) {
       try {
-        await deleteUser(authStore.userId); 
+        await deleteUser(authStore.userId);
         window.localStorage.clear();
-        setModalMessage('탈퇴 완료!');
+        setModalMessage("탈퇴 완료!");
         setIsModalOpen(true);
-        setRedirectPath('/'); 
+        setRedirectPath("/");
       } catch (error) {
         console.error(error);
-        setModalMessage('탈퇴 실패!');
+        setModalMessage("탈퇴 실패!");
         setIsModalOpen(true);
       }
     }
@@ -107,7 +110,7 @@ const Mypage = observer(() => {
   const closeModal = () => {
     setIsModalOpen(false);
     if (redirectPath) {
-      router.push(redirectPath); 
+      router.push(redirectPath);
     }
   };
 
@@ -123,14 +126,14 @@ const Mypage = observer(() => {
       reader.readAsDataURL(file);
 
       try {
-        const userId = authStore.userId; 
+        const userId = authStore.userId;
         const response = await uploadProfilePhoto(file, userId);
-        const filePath = response.profileUrl; 
+        const filePath = response.profileUrl;
         authStore.setProfileUrl(filePath);
         setProfileUrl(filePath);
-        setModalMessage('프로필 사진 업로드 완료!');
+        setModalMessage("프로필 사진 업로드 완료!");
       } catch (error) {
-        console.error('프로필 사진 업로드 오류:', error);
+        console.error("프로필 사진 업로드 오류:", error);
         setModalMessage(`프로필 사진 업로드 실패: ${error.message}`);
       }
       setIsModalOpen(true);
@@ -139,14 +142,16 @@ const Mypage = observer(() => {
 
   const handleImageDelete = async () => {
     try {
-      const userId = authStore.userId; 
+      const userId = authStore.userId;
       await deleteProfilePhoto(userId);
-      authStore.setProfileUrl("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+      authStore.setProfileUrl(
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+      );
       setProfileUrl(null);
-      setModalMessage('프로필 사진 삭제 완료!');
+      setModalMessage("프로필 사진 삭제 완료!");
     } catch (error) {
-      console.error('프로필 사진 삭제 오류:', error);
-      setModalMessage('프로필 사진 삭제 실패!');
+      console.error("프로필 사진 삭제 오류:", error);
+      setModalMessage("프로필 사진 삭제 실패!");
     }
     setIsModalOpen(true);
   };
@@ -162,81 +167,249 @@ const Mypage = observer(() => {
       </div>
       <div className="flex flex-col w-full max-w-md p-6 mt-5 overflow-y-auto bg-white rounded-lg shadow-md max-h-[70vh]">
         <div className="mb-4">
-          <label htmlFor="profileUrl" className="block mb-1 text-sm font-semibold text-gray-800">프로필 사진:</label>
-          <img src={profileUrl ? serverImage + profileUrl : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"} alt="Profile" className="w-32 h-32 mb-2 rounded-full" />
+          <label
+            htmlFor="profileUrl"
+            className="block mb-1 text-sm font-semibold text-gray-800"
+          >
+            프로필 사진:
+          </label>
+          <img
+            src={
+              profileUrl
+                ? serverImage + profileUrl
+                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            }
+            alt="Profile"
+            className="w-32 h-32 mb-2 rounded-full"
+          />
           <input
             type="file"
             id="profileUrl"
             name="profileUrl"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             accept="image/jpg,image/png,image/jpeg"
             onChange={handleFileChange}
             ref={fileInput}
           />
           <div className="flex justify-center mt-4">
-            <button type="button" className="w-full px-4 py-2 font-bold text-white bg-blue-400 rounded-full cursor-pointer" onClick={() => fileInput.current.click()}>프로필 사진 선택</button>
-            <button type="button" className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded-full cursor-pointer" onClick={handleImageDelete}>프로필 사진 삭제</button>
+            <button
+              type="button"
+              className="w-full px-4 py-2 font-bold text-white bg-blue-400 rounded-full cursor-pointer"
+              onClick={() => fileInput.current.click()}
+            >
+              프로필 사진 선택
+            </button>
+            <button
+              type="button"
+              className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded-full cursor-pointer"
+              onClick={handleImageDelete}
+            >
+              프로필 사진 삭제
+            </button>
           </div>
         </div>
 
         <form className={styles.form}>
           <div className="mb-4">
-            <label htmlFor="userId" className="block mb-1 text-sm font-semibold text-gray-800">아이디:</label>
-            <input type="text" id="userId" name="userId" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.userId} readOnly />
+            <label
+              htmlFor="userId"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              아이디:
+            </label>
+            <input
+              type="text"
+              id="userId"
+              name="userId"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.userId}
+              readOnly
+            />
           </div>
           <div className="mb-4">
-            <label htmlFor="userName" className="block mb-1 text-sm font-semibold text-gray-800">성명:</label>
-            <input type="text" id="userName" name="userName" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.userName} readOnly />
+            <label
+              htmlFor="userName"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              성명:
+            </label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.userName}
+              readOnly
+            />
           </div>
           <div className="mb-4">
-            <label htmlFor="nickName" className="block mb-1 text-sm font-semibold text-gray-800">닉네임:</label>
-            <input type="text" id="nickName" name="nickName" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.nickName} onChange={(e) => authStore.setNickName(e.target.value)} />
+            <label
+              htmlFor="nickName"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              닉네임:
+            </label>
+            <input
+              type="text"
+              id="nickName"
+              name="nickName"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.nickName}
+              onChange={(e) => authStore.setNickName(e.target.value)}
+            />
           </div>
-          {authStore.loginType === 'regular' && (
+          {authStore.loginType === "regular" && (
             <>
               <div className="mb-4">
-                <label htmlFor="currentPassword" className="block mb-1 text-sm font-semibold text-gray-800">현재 비밀번호:</label>
-                <input type="password" id="currentPassword" name="currentPassword" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
+                <label
+                  htmlFor="currentPassword"
+                  className="block mb-1 text-sm font-semibold text-gray-800"
+                >
+                  현재 비밀번호:
+                </label>
+                <input
+                  type="password"
+                  id="currentPassword"
+                  name="currentPassword"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
               </div>
               <div className="mb-4">
-                <label htmlFor="password" className="block mb-1 text-sm font-semibold text-gray-800">새 비밀번호:</label>
-                <input type="password" id="password" name="password" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.password} onChange={(e) => authStore.setPassword(e.target.value)} />
+                <label
+                  htmlFor="password"
+                  className="block mb-1 text-sm font-semibold text-gray-800"
+                >
+                  새 비밀번호:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+                  value={authStore.password}
+                  onChange={(e) => authStore.setPassword(e.target.value)}
+                />
               </div>
               <div className="mb-4">
-                <label htmlFor="confirmPassword" className="block mb-1 text-sm font-semibold text-gray-800">새 비밀번호 확인:</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.confirmPassword} onChange={(e) => authStore.setConfirmPassword(e.target.value)} />
+                <label
+                  htmlFor="confirmPassword"
+                  className="block mb-1 text-sm font-semibold text-gray-800"
+                >
+                  새 비밀번호 확인:
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+                  value={authStore.confirmPassword}
+                  onChange={(e) => authStore.setConfirmPassword(e.target.value)}
+                />
               </div>
             </>
           )}
           <div className="mb-4">
-            <label htmlFor="email" className="block mb-1 text-sm font-semibold text-gray-800">이메일:</label>
-            <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.email} readOnly />
+            <label
+              htmlFor="email"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              이메일:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.email}
+              readOnly
+            />
           </div>
           <div className="mb-4">
-            <label htmlFor="phone" className="block mb-1 text-sm font-semibold text-gray-800">전화번호:</label>
-            <input type="tel" id="phone" name="phone" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.phone} onChange={(e) => authStore.setPhone(e.target.value)} />
+            <label
+              htmlFor="phone"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              전화번호:
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.phone}
+              onChange={(e) => authStore.setPhone(e.target.value)}
+            />
           </div>
           <div className="mb-4">
-            <label className="block mb-1 text-sm font-semibold text-gray-800">성별:</label>
+            <label className="block mb-1 text-sm font-semibold text-gray-800">
+              성별:
+            </label>
             <div>
-              <input type="radio" id="male" name="gender" value="M" checked={authStore.gender === 'M'} onChange={(e) => authStore.setGender(e.target.value)} />
-              <label htmlFor="male" className="mr-2">남</label>
-              <input type="radio" id="female" name="gender" value="F" checked={authStore.gender === 'F'} onChange={(e) => authStore.setGender(e.target.value)} />
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="M"
+                checked={authStore.gender === "M"}
+                onChange={(e) => authStore.setGender(e.target.value)}
+              />
+              <label htmlFor="male" className="mr-2">
+                남
+              </label>
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="F"
+                checked={authStore.gender === "F"}
+                onChange={(e) => authStore.setGender(e.target.value)}
+              />
               <label htmlFor="female">여</label>
             </div>
           </div>
           <div className="mb-4">
-            <label htmlFor="birthDate" className="block mb-1 text-sm font-semibold text-gray-800">생년월일:</label>
-            <input type="date" id="birthDate" name="birthDate" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400" value={authStore.birthDate} onChange={(e) => authStore.setBirthDate(e.target.value)} />
+            <label
+              htmlFor="birthDate"
+              className="block mb-1 text-sm font-semibold text-gray-800"
+            >
+              생년월일:
+            </label>
+            <input
+              type="date"
+              id="birthDate"
+              name="birthDate"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-400"
+              value={authStore.birthDate}
+              onChange={(e) => authStore.setBirthDate(e.target.value)}
+            />
           </div>
           <div className="flex justify-center">
-            <button type="button" className="w-full px-4 py-2 font-bold text-white bg-teal-400 rounded-full cursor-pointer" onClick={handleUpdate}>수정</button>
-            <button type="button" className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded-full cursor-pointer" onClick={handleCancel}>탈퇴</button>
+            <button
+              type="button"
+              className="w-full px-4 py-2 font-bold text-white bg-teal-400 rounded-full cursor-pointer"
+              onClick={handleUpdate}
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              className="w-full px-4 py-2 font-bold text-white bg-red-400 rounded-full cursor-pointer"
+              onClick={handleCancel}
+            >
+              탈퇴
+            </button>
           </div>
         </form>
       </div>
       <Footer />
-      <Modal2 isOpen={isModalOpen} onClose={closeModal} title="알림" content={modalMessage} />
+      <Modal2
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        title="알림"
+        content={modalMessage}
+      />
     </div>
   );
 });
