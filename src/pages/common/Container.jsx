b9d3e91aacffd96b255310067464311b038495c2
msj102525 <msj102525@gmail.com>
2024-06-28 10:11:48 +0900

    Footer 추가 및 main pb-10 - 문승종

--- src/pages/common/Container.jsx
+++ src/pages/common/Container.jsx
@@ -4,7 +4,7 @@ import MainSection2 from "@/components/common/MainSection2";
 export default function Container() {
   return (
     <div className="flex justify-center w-full">
-      <div className="w-full max-w-screen-2xl">
+      <div className="w-full max-w-screen-2xl pb-20">
         <MainSection1 />
         <MainSection2 />
       </div>
